7dc53654e4cd4d2d6ac25318cacfb1eb3d32dda2
Martin_boulot <martin.schoreisz@cerema.fr>
2022-11-30 16:48:30 +0100

    recup par voie, sens, indic dans Trafic.recupDonneesTraficBase

--- src/Expoitation_mesures/Trafic.py
+++ src/Expoitation_mesures/Trafic.py
@@ -14,14 +14,52 @@ from Connexions.Connexion_Transfert import ConnexionBdd
 from Outils.Outils import checkParamValues, checkAttributsinDf
 
 
-def recupDonneesTraficBase(id_instru_site):
+def recupDonneesTraficBase(id_instru_site, sens=None, voie=None, indicateur=None):
     """
     telechager les donnees trafic source
+    in : 
+        id_instru_site : integer parmi les valeurs de la bdd
+        voie : list de string parmi les valeurs possible de la bdd
     """
-    with ConnexionBdd(bdd) as c:
+    dicoParams = {'sens': sens, 'voie': voie, 'indicateur': indicateur}
+    for k, v in dicoParams.items():
+        if v and not (isinstance(v, list)):
+            raise TypeError(f"{k} doit etre de type list")
+    if not voie and not indicateur and not sens:
         rqt = f"""SELECT *
                   FROM mesures_physiques.trafic
                   WHERE id_instru_site = {id_instru_site}"""
+    elif voie and indicateur and sens:
+        rqt = f"""SELECT *
+                  FROM mesures_physiques.trafic
+                  WHERE id_instru_site = {id_instru_site} and sens = any(ARRAY{sens}) and voie = any(ARRAY{voie}) and indicateur = any(ARRAY{indicateur})"""
+    elif sens and voie and not indicateur:
+        rqt = f"""SELECT *
+                  FROM mesures_physiques.trafic
+                  WHERE id_instru_site = {id_instru_site} and sens = any(ARRAY{sens}) and voie = any(ARRAY{voie})"""
+    elif sens and not voie and indicateur:
+        rqt = f"""SELECT *
+                  FROM mesures_physiques.trafic
+                  WHERE id_instru_site = {id_instru_site} and sens = any(ARRAY{sens}) and indicateur = any(ARRAY{indicateur})"""
+    elif sens and not voie and not indicateur:
+        rqt = f"""SELECT *
+                  FROM mesures_physiques.trafic
+                  WHERE id_instru_site = {id_instru_site} and sens = any(ARRAY{sens})"""
+    elif not sens and voie and not indicateur:
+        rqt = f"""SELECT *
+                  FROM mesures_physiques.trafic
+                  WHERE id_instru_site = {id_instru_site} and voie = any(ARRAY{voie})"""
+    elif not sens and voie and indicateur:
+        rqt = f"""SELECT *
+                  FROM mesures_physiques.trafic
+                  WHERE id_instru_site = {id_instru_site} and voie = any(ARRAY{voie}) and indicateur = any(ARRAY{indicateur})"""
+    elif not sens and not voie and indicateur:
+        rqt = f"""SELECT *
+                  FROM mesures_physiques.trafic
+                  WHERE id_instru_site = {id_instru_site} and indicateur = any(ARRAY{indicateur})"""
+    else:
+        raise NotImplementedError(f"la combinaison id_instru_site, voie, sens, indicateur n'est pas implémentée")
+    with ConnexionBdd(bdd) as c:
         df = pd.read_sql(rqt, c.sqlAlchemyConn)
     return df
 
